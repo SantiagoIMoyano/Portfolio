b355a31d517cae8c4a5f6d6e7bd21434af39a719
Santiago Ismael Moyano <86203@sistemas.frc.utn.edu.ar>
2025-02-11 12:00:21 -0300

    Prueba push imágen en Docker Hub y despliegue en Render

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+# Eliminar seeders previos 
+npx sequelize-cli db:seed:undo:all
+
+#Eliminar migraciones previas
+npx sequelize-cli db:migrate:undo:all
+
 # Ejecuta las migraciones
 npx sequelize-cli db:migrate
 
