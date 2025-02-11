5bbd0d6b0b4754cb778963b9fab3d315fa55323e
Santiago Ismael Moyano <86203@sistemas.frc.utn.edu.ar>
2025-02-11 11:30:55 -0300

    Cambios en entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "Esperando a que la base de datos esté disponible..."
-while ! nc -z db 3306; do
-  sleep 5
-done
-echo "La base de datos está disponible."
-
 # Ejecuta las migraciones
 npx sequelize-cli db:migrate
 
