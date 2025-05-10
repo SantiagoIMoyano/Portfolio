d6a35543b39a8ae2df0c6e07fdddd831acaa162b
Santiago Ismael Moyano <86203@sistemas.frc.utn.edu.ar>
2025-05-10 20:57:09 -0300

    Update portfolio

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 set -e
 
-# Eliminar seeders previos
 npx sequelize-cli db:seed:undo:all
 
-#Eliminar migraciones previas
 npx sequelize-cli db:migrate:undo:all
 
-# Ejecuta las migraciones 
 npx sequelize-cli db:migrate
 
-# Ejecuta los seeders
 npx sequelize-cli db:seed:all
